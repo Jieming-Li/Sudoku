cd36d282ab7202540c391dcb11c6106e0689a53b
Jimmy <jimmyli7099@gmail.com>
2024-01-08 18:26:14 -0600

    finished implementation of user interface

--- src/sudoku/problemdomain/SudokuGame.java
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -1,12 +1,14 @@
 package sudoku.problemdomain;
 
+import sudoku.constants.GameState;
+
 import java.io.Serializable;//read and write this particular game data file to a file on the operating system easier
 
 public class SudokuGame implements Serializable{
     private final GameState gameState;
     private final int[][] girdState;
 
-    public static final int GRID_BOUNDRY = 9;
+    public static final int GRID_BOUNDARY = 9;
     public SudokuGame(GameState gameState, int[][] girdState) {
         this.gameState = gameState;
         this.girdState = girdState;
@@ -16,7 +18,7 @@ public class SudokuGame implements Serializable{
         return gameState;
     }
 
-    public int[][] getCopyOfGirdState() {
+    public int[][] getCopyOfGridState() {
         return SudokuUtilities.copyToNewArray(girdState);
     }
 }
